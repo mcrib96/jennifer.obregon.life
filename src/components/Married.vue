9a8cd0917d674571d410bd6638a8dae45bf2ada3
Dan Obregon <dan.obregon@destpet.com>
2023-08-03 08:47:57 -0500

    Married: carousel card - married

---
+++ src/components/Married.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="mx-16 ">
+        <v-row class="text-center">
+            <v-col>
+                <h3>Happily Married</h3>
+            </v-col>
+        </v-row>
+        <v-row class="d-flex flex-row justify-center">
+            <v-col cols="3">
+                <p>
+                    After dating for several years and not even knowing about it, we got married in 2010.
+                    With dozens of chaperones (often at the same time) watching, our friendship began
+                    and blossomed into a family that we could never have imagined.  Throughout our time
+                    together, God has been and continues to be the center of it all.  Without Him, 
+                    our life would be ordinary.
+                </p>
+            </v-col>
+            <v-col cols="6">
+                <v-img src="/assets/dan+jenni.jpg" class="rounded" />
+            </v-col>
+        </v-row>
+    </div>
+</template>

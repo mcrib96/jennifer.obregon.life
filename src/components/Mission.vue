b0582b1bc7cd115a1ae0206542359985112fa6c8
Dan Obregon <dan.obregon@destpet.com>
2023-08-03 08:47:46 -0500

    Mission: carousel card - mission statement

---
+++ src/components/Mission.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="mx-16 ">
+        <v-row class="text-center">
+            <v-col>
+                <h3>Personal Mission Statement</h3>
+            </v-col>
+        </v-row>
+        <v-row class="d-flex flex-row justify-center">
+            <v-col cols="3">
+                <v-img src="/assets/portrait.jpg" class="rounded" />
+            </v-col>
+            <v-col cols="6">
+                <p >
+                To live, as well as love and serve others, out of total dependency on the life of Christ within me.
+                Through Jesus I have live and value and an unconditional "Yes" of approval by the God of Heaven.
+                Nothing else is needed.
+                </p>
+                <br>
+                <i>
+                    And I am convinced that nothing can ever separate us from God's love.
+                    Neither death, nor life, neither angles nor demons, neither our fears
+                    for today, nor our worries about tomorrow - not even the powers of 
+                    hell can separate us from God's love. Romans 8:38
+                </i>
+            </v-col>
+        </v-row>
+    </div>
+</template>

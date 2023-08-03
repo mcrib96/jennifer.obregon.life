f137ea71d75f043ffd706888f155c949ccd1bed8
Dan Obregon <dan.obregon@destpet.com>
2023-08-03 08:47:12 -0500

    All: single page with all components

---
+++ src/pages/All.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <Home/>
+        <About />
+        <Connect />
+    </div>
+</template>
+<script lang="ts" setup>
+import About from './About.vue'
+import Home from './Home.vue'
+import Connect from './Connect.vue'
+</script>

043a00e73d441aae3e506ce3b3ff7bd99720eb76
Dan Obregon <dan.obregon@destpet.com>
2023-08-03 08:48:17 -0500

    Children: carousel card - children

---
+++ src/components/Children.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="mx-16 ">
+        <v-row class="text-center">
+            <v-col>
+                <h3>Wonderful Children</h3>
+            </v-col>
+        </v-row>
+        <v-row class="d-flex flex-row justify-center align-top">
+            <v-col cols="3" class="ma-0 pa-0">
+                <v-img src="/assets/sarah+michael.jpg" class="rounded" />
+            </v-col>
+            <v-col cols="3">
+                <p>
+                    I have two wonderful children: Sarah and her husband Michael and Joshua and his wife Kaitlin.
+                </p>
+            </v-col>
+            <v-col cols="3" class="ma-0 pa-0">
+                <v-img src="/assets/josh+kaitie.jpg" class="rounded" />
+            </v-col>
+        </v-row>
+    </div>
+</template>

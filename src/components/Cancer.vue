2a272866957ef20528a28fce975c2eb0bf726e54
Dan Obregon <dan.obregon@destpet.com>
2023-08-03 08:47:32 -0500

    Cancer: carousel card - cancer

---
+++ src/components/Cancer.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="mx-16 ">
+        <v-row class="text-center">
+            <v-col>
+                <h3>More Than Pink</h3>
+            </v-col>
+        </v-row>
+        <v-row class="d-flex flex-row justify-center">
+            <v-col cols="6">
+                <v-img src="/assets/cancer-battle.jpg" class="rounded" />
+            </v-col>
+            <v-col cols="3">
+                <p>
+                    In 2014, my battle with breast cancer began.  With prayers and support
+                    from family, friends, new friends, and the Grace of God, I won that battle.
+                    God is now using this experience in ways that I could never have imagined
+                    bringing comfort and a voice of experience to those now fighting their 
+                    own battles.
+                </p>
+            </v-col>
+        </v-row>
+    </div>
+</template>

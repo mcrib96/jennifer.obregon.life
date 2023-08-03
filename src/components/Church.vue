3d39789aca497d9d5ca22ab8b007eef04d60f3a0
Dan Obregon <dan.obregon@destpet.com>
2023-08-03 08:48:06 -0500

    Church: carousel card - church

---
+++ src/components/Church.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="mx-16 ">
+        <v-row class="text-center">
+            <v-col>
+                <h3>Cottonwood Creek Church</h3>
+            </v-col>
+        </v-row>
+        <v-row class="d-flex flex-row justify-center">
+            <v-col cols="3">
+                <p>
+                    On Saturday nights, you'll find us at Cottonwood Creek church in Allen, Texas, in an intimate worship
+                    experience where Pastor Graeme Golding shares the message God has given him.<br>
+                    <br>
+                    Sunday mornings, we study the Bible with the Dresser / Pool life group and then serve
+                    in the Creek Kids ministry where my husband and I teach the first grade.
+                </p>
+            </v-col>
+            <v-col cols="6">
+                <v-img src="/assets/cottonwood_creek.jpg" class="rounded" />
+            </v-col>
+        </v-row>
+    </div>
+</template>

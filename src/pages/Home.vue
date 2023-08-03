239ec9a0f680438d1a600e8ba9581b209fb1dbbe
Dan Obregon <dan.obregon@destpet.com>
2023-08-03 08:46:17 -0500

    Home: home page background image, name, title

---
+++ src/pages/Home.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="background ma-10 pa-10">
+        <p class="name">
+            Jennifer A. Obregon<br/>
+            Commercial Lines Account Manager
+        </p>
+    </div>
+</template>
+<style scoped>
+ .background {
+    background-image: url('../assets/background.jpg');
+    width: 80vw;
+    height: 85vh;
+    opacity: 0.6;
+    border: 1px solid #999999;
+    border-radius: 20px;
+ }
+ .name {
+    text-align: center;
+    font-weight: bold;
+    font-size: 32px;
+    padding-top: 30vh;
+ }
+</style>
